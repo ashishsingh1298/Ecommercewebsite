cfdd898b0dbd6975333017212320ded2d2445810
Ashish Singh <54949523+ashishsingh1298@users.noreply.github.com>
2020-09-18 13:24:11 -0700

    Important changes
    these are the irrellevent file which i dont need so i deleted

--- cart/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Cart
-# Register your models here.
-
-class CartAdmin(admin.ModelAdmin):
-	class Meta:
-		model = Cart
-
-admin.site.register(Cart, CartAdmin)
